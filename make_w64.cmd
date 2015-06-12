2b3549274c41e63af9355427154939b440fcd301
unknown <rj150@hotmail.com>
2015-06-11 23:10:09 -0400

    Direct the WDK toolchain interface to the un-compiled directories.

--- make_w64.cmd
+++ make_w64.cmd
@@ -3,13 +3,14 @@
 REM set target=i386
 set target=amd64
 
-set MSVC=%USERPROFILE%\cmsc\msvc\bin\%target%
-set libs=/LIBPATH:"%MSVC%\..\..\lib\%target%"
-set incl=/I"%MSVC%\..\..\include\crt"
+set DDK=C:\WinDDK\7600.16385.1
+set MSVC=%DDK%\bin\x86\%target%
+set incl=/I"%DDK%\inc\crt" /I"%DDK%\inc\api"
+set libs=/LIBPATH:"%DDK%\lib\crt\%target%"
 
 set C_FLAGS=/Wall /O1 /Os /Ob1 /Oi /GS- /MD /TC
 set LINK_FLAGS=%libs% /ENTRY:"main" /FILEALIGN:512 /MERGE:.rdata=.text
 
-%MSVC%\cl.exe %CD%\spconfig.c %incl% %C_FLAGS% %EXTRA_LIBS% /link %LINK_FLAGS%
+%MSVC%\cl.exe %CD%\spconfig.c %incl% %C_FLAGS% /link %LINK_FLAGS%
 
 pause
