de62d2dc6f597bc3742e2f22c1edf0bba522619a
unknown <rj150@hotmail.com>
2015-12-03 11:14:45 -0500

    removed accidental inclusion of unneeded /lib/wnet path

--- make_w32.cmd
+++ make_w32.cmd
@@ -6,7 +6,7 @@ REM set target=amd64
 set DDK=C:\WinDDK\7600.16385.1
 set MSVC=%DDK%\bin\x86\x86
 set incl=/I"%DDK%\inc\crt" /I"%DDK%\inc\api"
-set libs=/LIBPATH:"%DDK%\lib\crt\%target%" /LIBPATH:"%DDK%\lib\wnet\%target%"
+set libs=/LIBPATH:"%DDK%\lib\crt\%target%"
 
 set C_FLAGS=/Wall /Ox /Ob1 /Oi /GS- /MD /TC
 set LINK_FLAGS=%libs% /ENTRY:"main" /FILEALIGN:512 /MERGE:.rdata=.text
